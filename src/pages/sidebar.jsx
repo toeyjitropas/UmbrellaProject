7a3296c06f26c592dd740cb559ac4a563a122257
Rathwathra Jitropas <toey.jitropas@gmail.com>
2023-10-14 23:18:30 +0700

    Doing animation of sidebar

--- src/pages/sidebar.jsx
+++ src/pages/sidebar.jsx
@@ -10,7 +10,7 @@ function Sidebar({ userRole, page, status }) {
         <div className='h-16'>
             <h1 className="text-xl font-semibold p-4 text-gray-950 align-middle">
             <FaCarOn className="inline-block mr-4 align-middle" />
-            <span className={`${status ? 'ease-in-out duration-150 hidden' : 'after:duration-150 after:ease-in-out after:delay-700'}`}> SIT Middle Office
+            <span className={`${status ? ' hidden' : ''}`}> SIT Middle Office
               </span></h1>
         </div>
       <ul>
@@ -18,7 +18,7 @@ function Sidebar({ userRole, page, status }) {
           .filter(item => item.role.includes(userRole)) // Filter menu items based on user role
           .map((item) => (
             <li key={item.topic} className="border-t border-gray-200">
-              {item.topicName && <h2 className={`${status ? 'm-4':'p-6'} text-lg font-semibold text-gray-900 ease-in-out duration-150`}>{status ? item.topicInit : item.topicName}</h2>}
+              {item.topicName && <h2 className={`${status ? 'm-4':'p-6'} text-lg font-semibold text-gray-900 `}>{status ? item.topicInit : item.topicName}</h2>}
               <ul>
                 {item.menu
                     .filter(item => item.role.includes(userRole))
@@ -30,7 +30,7 @@ function Sidebar({ userRole, page, status }) {
                             : 'hover:bg-indigo-50 text-gray-800'
                         }`}> 
                           {menuItem.icon && <menuItem.icon className={`${status ? 'm-auto' : "flex mr-4"}`} />}
-                          <div className={`${status ? 'hidden' : 'delay-700'}`}>{menuItem.name}</div> 
+                          <div className={`${status ? 'hidden' : ''}`}>{menuItem.name}</div> 
                         </li>
                         </Link>
                     ))}
